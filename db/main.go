96e6c1400c8ecd136c5b81c5b8d0f77bb1ad15c9
david <david.huang@noovo.co>
2019-03-30 09:39:13 +0800

    first commit

---
+++ db/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"cloud/database"
+	"cloud/lib/logger"
+	"cloud/server/ota/config"
+	"database/sql"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+func conn(n int) {
+	for i := 1; i <= n; i++ {
+		config.ParseConfig(os.Getenv("GOPATH")+"/src/cloud/server/ota/config/config.local.json", &config.Config)
+		cfg := &config.Config
+
+		dbConfig := config.GetPgsqlConfig(cfg.DB)
+		db, err := database.Connect(dbConfig)
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+		logger.Debug(i, db)
+	}
+}
+
+func read(db *sql.DB, i int) {
+	rows, err := db.Query(`SELECT * FROM cloud.device_stat_vod`)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	defer rows.Close()
+	// fmt.Println(i)
+}
+
+func goread(db *sql.DB, i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	rows, err := db.Query(`SELECT * FROM cloud.device_stat_vod`)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	defer rows.Close()
+	// fmt.Println(i)
+}
+
+func chread(db *sql.DB, in, out chan int) {
+
+	go func() {
+		for i := range in {
+			func() {
+
+				rows, err := db.Query(`SELECT * FROM cloud.device_stat_vod`)
+				if err != nil {
+					logger.Error(err)
+					return
+				}
+				defer rows.Close()
+				out <- i
+			}()
+
+		}
+	}()
+}
+
+func main() {
+
+	config.ParseConfig(os.Getenv("GOPATH")+"/src/cloud/server/ota/config/config.local.json", &config.Config)
+	cfg := &config.Config
+
+	dbConfig := config.GetPgsqlConfig(cfg.DB)
+
+	db, err := database.Connect(dbConfig)
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	n := 200
+
+	t1 := time.Now()
+	for i := 1; i <= n; i++ {
+		read(db, i)
+	}
+	t2 := time.Now()
+	d1 := t2.Sub(t1)
+
+	fmt.Println("---------------------------------------")
+
+	wg := &sync.WaitGroup{}
+
+	t1 = time.Now()
+	for i := 1; i <= n; i++ {
+		wg.Add(1)
+		go goread(db, i, wg)
+	}
+	wg.Wait()
+	t2 = time.Now()
+	d2 := t2.Sub(t1)
+
+	fmt.Println("---------------------------------------")
+	in := make(chan int, 50)
+	out := make(chan int)
+	for i := 1; i <= 2; i++ {
+		chread(db, in, out)
+	}
+
+	t1 = time.Now()
+	go func() {
+		for i := 1; i <= n; i++ {
+			in <- i
+		}
+		defer close(in)
+	}()
+	for i := 1; i <= n; i++ {
+		// fmt.Println(<-out)
+		<-out
+	}
+	t2 = time.Now()
+	d3 := t2.Sub(t1)
+
+	fmt.Println(d1, d2, d3)
+
+	conn(50)
+}
---
+++ pointer/receiver.go
@@ -0,0 +1,34 @@
+package main
+
+import "cloud/lib/logger"
+
+type S1 struct {
+	A int
+	B *int
+}
+
+func (s *S1) SetA(i int) {
+	s.A = i
+	logger.Debug(s)
+	logger.Debugf(`%p`, s)
+}
+
+func SetA(s S1, i int) {
+	s.A = i
+	logger.Debugf(`%p`, &s)
+}
+
+func main() {
+	var s1 S1
+	s2 := &s1
+	s3 := *s2
+	s4 := *s2
+	s5 := s2
+
+	s1.SetA(1)
+
+	SetA(s1, 2)
+
+	logger.Debug(s1, s2)
+	logger.Debugf(`%p, %p, %p, %p, %p, %p`, &s1, s2, &s3, &s4, s5, s1.B)
+}
