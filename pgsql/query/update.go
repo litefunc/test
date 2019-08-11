4f8066f2e9c0b7ffac2262a58a4856b312f4ee41
litefunc <lite.func@gmail.com>
2019-08-11 22:28:10 +0800

    update pgsql

--- pgsql/query/update.go
+++ pgsql/query/update.go
@@ -1,1 +1,21 @@
 package query
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (q Query) Update(tb string, set map[string]interface{}) Query {
+	var cols []string
+	var args []interface{}
+	for k := range set {
+		cols = append(cols, fmt.Sprintf(`%s=?`, k))
+		args = append(args, set[k])
+	}
+
+	sql := fmt.Sprintf(`%s %s SET %s`, UPDATE.String(), tb, strings.Join(cols, ", "))
+	q.sql = append(q.sql, sql)
+	q.args = append(q.args, args...)
+
+	return q
+}
