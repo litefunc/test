84975d9d32a65e0f14dbb2f0a19700f7ac1de09d
david <david.huang@noovo.co>
2019-10-21 09:15:12 +0800

    update os

--- path/main.go
+++ path/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 	"path"
 )
 
 func main() {
 	fmt.Println(path.Join("a", "b", "/c/"))
 	fmt.Println(path.Join("/a", "b", "/c/"))
+	fmt.Println(path.Join("http://localhost:8080", "b", "/c/"))
+
+	u, err := url.Parse("//")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	u.Path = path.Join(u.Path, "bar.html")
+	s := u.String()
+	log.Println(s)
+
 }
