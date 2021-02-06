0b9e7b721ad75923f4a3c33fce434ac648be9e60
david <david.huang@noovo.co>
2021-02-06 23:20:52 +0800

    update

--- channel/main.go
+++ channel/main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	"cloud/lib/logger"
-	"time"
+	"test/logger"
 )
 
 func main() {
-	// ch := make(chan int)
-
-	// go func() {
-	// 	ch <- 1
-	// 	time.Sleep(time.Second * 2)
-	// 	close(ch)
-	// }()
-
-	// time.Sleep(time.Second)
-
-	// logger.Debug(<-ch)
-
-	// logger.Debug(<-ch)
-
-	// for i := range ch {
-	// 	logger.Debug(i)
-	// }
-	ch1 := make(chan []*int)
-	var is []*int
-	for i := 0; i < 10; i++ {
-		is = append(is, &i)
-	}
+	ch := make(chan int)
 
 	go func() {
-		ch1 <- is
-		is = nil
+		ch <- 1
+		close(ch)
+
 	}()
-	time.Sleep(time.Second)
-	got := <-ch1
-	logger.Debug(len(got), got)
+
+	logger.Debug(<-ch)
+	logger.Debug(<-ch)
+	logger.Debug(<-ch)
+
+	for i := range ch {
+		logger.Debug(i)
+	}
+	ch <- 2
+
+	// ch1 := make(chan []*int)
+	// var is []*int
+	// for i := 0; i < 10; i++ {
+	// 	is = append(is, &i)
+	// }
+
+	// go func() {
+	// 	ch1 <- is
+	// 	is = nil
+	// }()
+	// time.Sleep(time.Second)
+	// got := <-ch1
+	// logger.Debug(len(got), got)
 }
