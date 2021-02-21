a024515fe0a1f4f0414013568ff869ff7991a5f4
david <david.huang@noovo.co>
2021-02-21 23:15:41 +0800

    update

--- interface/main.go
+++ interface/main.go
@@ -1,25 +1,56 @@
 package main
 
-type a interface {
-	a()
+import (
+	"test/logger"
+)
+
+type Eq interface {
+	eq(Eq) bool
+	val() interface{}
 }
 
-type b struct{}
+type Int int
 
-func (rec b) a() {
+func (rec Int) val() interface{} {
 
+	return rec
 }
 
-func pa(a) {
-
+func (rec Int) eq(e Eq) bool {
+	i, ok := e.val().(Int)
+	if !ok {
+		return false
+	}
+	return i == rec
 }
 
-func p([]a) {
+// func (rec Int) eq(e Eq) bool {
+// 	i, ok := e.(int)
+// 	return reflect.DeepEqual(rec, e)
+// }
 
+func unique(es ...Eq) []Eq {
+	if len(es) == 0 {
+		return []Eq{}
+	}
+	list := []Eq{}
+	m := make(map[Eq]bool)
+	for _, v := range es {
+		if _, ok := m[v]; !ok {
+			m[v] = true
+			list = append(list, v)
+		}
+	}
+
+	return list
 }
 
 func main() {
-	var bs []b
-	pa(b{})
-	p(bs)
+	i := Int(1)
+	j := Int(1)
+	k := Int(2)
+	logger.Debug(i.eq(j))
+	logger.Debug(i.eq(k))
+
+	logger.Debug(unique(i, j, k))
 }
